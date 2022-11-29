e0e118a15c6d5a1bb0c0505c7b924c1c44f2e766
JhormaMalave <jhormamalave99@gmail.com>
2022-11-29 02:35:34 -0400

    Finish article form component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 // Tailwind Styles
 require('./index.css')
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
