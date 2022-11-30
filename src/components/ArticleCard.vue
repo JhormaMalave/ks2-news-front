ae3c6250675020bd335b1bc2c8dea0be544dfeac
JhormaMalave <jhormamalave99@gmail.com>
2022-11-29 20:34:23 -0400

    Add route link on cards and create navbar

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,11 +1,11 @@
 <template>
-    <a href="#" class="overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
+    <router-link :to="'articles/' + id" class="overflow-hidden rounded-xl bg-white shadow-md duration-200 hover:scale-105 hover:shadow-xl">
       <img src="https://i.imgur.com/5dmBrx6.jpg" alt="plant" class="h-auto w-full" />
       <div class="p-5">
         <p class="text-gray-900 font-bold text-2xl tracking-tight mb-1 dark:text-white">{{ title }}</p>
         <p class="text-medium mb-5 text-gray-700">{{ dateTime(createdAt) }}</p>
       </div>
-    </a>
+    </router-link>
 </template>
 
 <script>
@@ -15,7 +15,8 @@ export default {
   name: 'ArticleCard',
   props: {
     title: String,
-    createdAt: String
+    createdAt: String,
+    id: Number
   },
   methods: {
     dateTime(value) {
