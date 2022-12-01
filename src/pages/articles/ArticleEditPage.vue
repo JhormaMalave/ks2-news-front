6be371e2ffb888f9726d243cc47a27d299ea3a27
JhormaMalave <jhormamalave99@gmail.com>
2022-11-30 23:54:02 -0400

    Get article from api

--- src/pages/articles/ArticleEditPage.vue
+++ src/pages/articles/ArticleEditPage.vue
@@ -4,16 +4,40 @@
       <h1 class="text-2xl font-extrabold text-gray-600 sm:text-3xl md:text-5xl mb-7">
         <span class="text-indigo-400">Editar</span> noticia.
       </h1>
-      <ArticleForm />
+      <ArticleForm
+        :defaultArticle="article"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import ArticleForm from '@/components/ArticleForm.vue';
+import axios from 'axios';
 
 export default {
   name: 'ArticleEditPage',
-  components: {ArticleForm}
+  components: {ArticleForm},
+  data() {
+    return {
+      id: this.$route.params.id,
+      article: null
+    }
+  },
+  methods: {
+    async getArticle () {
+      try {
+        const response = await axios.get(
+          `http://localhost:3000/api/articles/${this.id}`
+        );
+        this.article = response.data;
+        console.log(response.data)
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }, created () {
+    this.getArticle();
+  }
 }
 </script>
